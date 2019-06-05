446b1e74b27f24931461bc0eb2e18b5167bbc37c
Fajar Septianto <49784833+F4JAR-ID@users.noreply.github.com>
2019-06-05 19:31:41 +0700

    Create Voucher.sh

---
+++ Voucher.sh
@@ -0,0 +1,23 @@
+#! / usr / bin / bash
+# tools kode voucher
+# oleh acep
+bersih
+biru = ' \ 033 [34; 1 m '
+hijau = ' \ 033 [32; 1 m '
+ungu = ' \ 033 [35; 1 m '
+cyan = ' \ 033 [36; 1m '
+red = ' \ 033 [31; 1m '
+gema " "
+echo $ blue " <| ====================================== |> "
+echo $ red " | TOOLS Hack Voucher | "
+echo $ red " | | "
+echo $ red " | Penulis: Acep X-Code | "
+echo $ red " | Kontak: 085797616471 | "
+echo $ red " | Tim Saya: Tim Indo | "
+echo $ red " | Terima Kasih Kepada: Holilul Anwar | "
+echo $ blue " <| ====================================== |> "
+tidur 1
+gema " "
+echo $ green " ============== $ red MENU $ green ================= "
+echo $ blue " ================================== "
+echo $ blue " || $ hijau 1. Indosat $ blue || " 
